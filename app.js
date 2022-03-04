f2730bfaccdf6eb6652c07418089527882fee805
SrGiovanni <samuelefulghi@gmail.com>
2022-03-04 12:42:05 -0500

    add modular webpage generator

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [username, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(username, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
